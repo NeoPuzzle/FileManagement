3615af71f118f3bb252b144fac68765caf176e34
rbenites <alrrebe@gmail.com>
2024-11-17 23:57:22 -0500

    test create files implemented

--- backend/src/files/files.controller.spec.ts
+++ backend/src/files/files.controller.spec.ts
@@ -4,24 +4,30 @@ import { FilesService } from './files.service';
 import { INestApplication } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { CreateFileDto } from './files.dto';
+import * as request from 'supertest';
 
 
 describe('FilesController', () => {
   let app: INestApplication;
   let service: FilesService;
   let jwtService: JwtService;
+  let validToken: string;
 
   beforeAll(async () => {
+    process.env.JWT_SECRET = 'unaclavesecreta';
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
       providers: [
         { provide: FilesService, 
           useValue: {
-
+            // canActivate: jest.fn(() => true),
+            createFile: jest.fn(),
           }
         },
         JwtService,
-        AuthGuard,
+        AuthGuard
       ],
       imports: [
         JwtModule.register({
@@ -35,10 +41,30 @@ describe('FilesController', () => {
     await app.init();
     service = module.get<FilesService>(FilesService);
     jwtService = module.get<JwtService>(JwtService);
+
+    validToken = jwtService.sign({ userId: '1'});
   });
 
   it('debería estar definido', () => {
     const controller = app.get<FilesController>(FilesController);
     expect(controller).toBeDefined();
+  });
+
+  it('deberia crear un nuevo archivo, con token de autorizacion', async() => {
+    const createFileDto: CreateFileDto = { weight: 3.2, quantity: 5, type: 'pdf' };
+    const result = { ...createFileDto, id: '1'};
+
+    jest.spyOn(service, 'createFile').mockResolvedValue(result);
+
+    return request(app.getHttpServer())
+    .post('/files')
+    .set('authorization', `Bearer ${validToken}`)
+    .send(createFileDto)
+    .expect(201, result);
+
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
